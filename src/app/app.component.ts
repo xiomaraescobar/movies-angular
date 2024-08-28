12a093b728708ac0f98f1a86966d416212da3de7
xiomaraescobar <161639652+xiomaraescobar@users.noreply.github.com>
2024-08-28 22:02:16 +0000

    agregando css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ type Movies = Array<Movie>;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title: String = 'Peliculas';
